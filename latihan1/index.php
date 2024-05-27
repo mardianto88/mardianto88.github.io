cd54b49ad11463c7b97053cd860b2e10690b90cf
MARDIANTO <56868836+mardianto88@users.noreply.github.com>
2024-05-27 10:44:40 +0800

    honor

--- latihan1/index.php
+++ latihan1/index.php
@@ -1,1 +1,10 @@
 
+<?php
+//deklarasi variabel
+$nama_lengkap = "Alfred Futterkiste"; //string
+$honor = 1000000; //integer
+$bonus = 0.5; //float
+echo "Nama Lengkap: $nama_lengkap <br>";
+echo "Honor: Rp. $honor,- <br>";
+echo "Bonus: $bonus x Honor <br>";
+?>
